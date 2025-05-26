8498a5b92a58413fe77317a663ddff28a78f01ed
Muhamad-Ali-13 <135040640+Muhamad-Ali-13@users.noreply.github.com>
2025-05-26 11:19:35 +0700

    N

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('role'); // Default role is 'user'
+            $table->string('role')->default('U'); // Default role is 'user'
             $table->rememberToken();
             $table->timestamps();
         });
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -34,17 +34,17 @@
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2 text-sm text-red-600 dark:text-red-500" />
         </div>
 
-        <!-- Role -->
+        {{-- <!-- Role -->
         <div>
             <label for="role" class="block text-sm font-medium text-gray-700 dark:text-gray-300">Role</label>
             <select id="role" name="role"
                 class="mt-1 block w-full px-3 py-2 bg-gray-50 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm dark:bg-gray-700 dark:border-gray-600 dark:text-gray-300">
                 <option value="" disabled selected>Pilih Role</option>
-                {{-- <option value="A">Admin</option> --}}
+                <option value="A">Admin</option> 
                 <option value="U">User</option>
                 <option value="K">Konsultan</option>
             </select>
-        </div>
+        </div> --}}
 
         <!-- Action Buttons -->
         <div class="flex flex-col sm:flex-row items-center justify-between mt-4 space-y-4 sm:space-y-0">
